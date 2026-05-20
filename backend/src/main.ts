158853e988d3bc11e297372a49fbbd5a9e7da837
Victor Lopes <victor.lopesms@gmail.com>
2026-05-20 18:14:39 -0300

    feat: initialize monorepo structure with NestJS and Next.js

---
+++ backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter({ logger: true }));
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
