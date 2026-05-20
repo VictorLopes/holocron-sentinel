22c231cff80d4651c62f5245d0ba807efbf218d9
Victor Lopes <victor.lopesms@gmail.com>
2026-05-20 18:26:02 -0300

    fix: env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 
+
+try {
+  process.loadEnvFile();
+} catch (error) {
+  console.warn(".env is missing")
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter({ logger: true }));
   await app.listen(process.env.PORT ?? 3001);
