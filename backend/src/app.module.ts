fd163f480734e270f6d06e17413de425766d83fd
Victor Lopes <victor.lopesms@gmail.com>
2026-05-21 00:53:00 -0300

    refactor: remove default boilerplate app controller and service files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { EntitiesModule } from './entities/entities.module';
 import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [DatabaseModule, EntitiesModule, EventsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
